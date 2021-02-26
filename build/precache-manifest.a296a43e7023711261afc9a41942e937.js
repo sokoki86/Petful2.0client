949f5b2eca1f1852acf5ed12a64320b8f6d66c19
Jessica <jmbaskerville1217@gmail.com>
2021-02-26 12:16:42 -0500

    pushing readmes

---
+++ build/precache-manifest.a296a43e7023711261afc9a41942e937.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7421271d154f2119c22d74a9dd47d37c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "14e519b7ce8875f1b099",
+    "url": "/static/css/main.e18c8514.chunk.css"
+  },
+  {
+    "revision": "2064c6564ca5768b9472",
+    "url": "/static/js/2.594f811d.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.594f811d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14e519b7ce8875f1b099",
+    "url": "/static/js/main.54076600.chunk.js"
+  },
+  {
+    "revision": "72c5965cbfb3ac6a55d7",
+    "url": "/static/js/runtime-main.40d8ffab.js"
+  }
+]);
--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    // API_ENDPOINT: `https://petful-server.herokuapp.com/api`,
-    API_ENDPOINT: 'http://localhost:8000/api'
+    API_ENDPOINT: `https://lit-hollows-28571.herokuapp.com/api`,
+    // API_ENDPOINT: 'http://localhost:8000/api'
   };
